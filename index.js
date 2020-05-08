c94b8036186c3f83d6a9acc6698abedfb21791e6
iths-anna-boberg <amboberg@gmail.com>
2020-05-08 12:53:05 +0200

    hamster routes
    fixat två get-metoder

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+
+
 app.use(express.static('public')); //serva en enkel htmlsida for now
 app.use(express.json());
+
+
+//routes
+const hamstersRoute = require('./routes/hamsters');
+app.use('/hamsters', hamstersRoute);
 
 app.listen(3000, ()=>{
     console.log('Server up and running @ port 3000');
--- routes/hamsters.js
+++ routes/hamsters.js
@@ -1,7 +1,52 @@
 const { Router } = require('express');
 const router = new Router();
-
-
 const { db } = require ('./../firebase');
+
+//GET Returnerar en array med samtliga hamsterobject från firestore
+
+router.get('/', async (req, res)=>{
+    
+    try{
+        
+        let querySnapshot = await db.collection('hamsters').get();
+        let arr = [];
+        
+        querySnapshot.forEach(el =>{ //loopa igenom resultatet och skapa array av alla objekt
+            arr.push(el.data())
+        })
+        
+        res.send(arr)
+        
+    }catch(err){
+        res.status(500).send(err)
+    }
+    
+})
+
+//GET ID Returnerar ett objekt utifrån hamsterns id
+
+router.get('/:id', async (req, res)=>{
+    let id = req.params.id*1; //gör om till integer
+    let result ={} //obj behövs för att kunna skicka resultat
+    try{
+        
+        let querySnapshot = await db.collection('hamsters').where('id', '==', id).get();
+        querySnapshot.forEach(el=>{
+            result = el.data();
+        })
+        res.send(result);
+        
+    }catch(err){
+
+        res.status(500).send(err)
+        
+    }
+    
+    let querySnapshot = await db.collection('hamsters').where('id', '==', id).get();
+    querySnapshot.forEach(el=>{
+        result = el.data();
+    })
+    res.send(result);
+})
 
 module.exports = router;
