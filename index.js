eb5184cc24a7a0a7e25af4d731c0acf4de9efeb5
iths-anna-boberg <amboberg@gmail.com>
2020-05-08 11:02:07 +0200

    La till basic front end
    irriterade mig på cannot GET i webbläsaren så jag la till en enkel frontend bara för att slippa det.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+app.use(express.static('public')); //serva en enkel htmlsida for now
+app.use(express.json());
 
 app.listen(3000, ()=>{
     console.log('Server up and running @ port 3000');
