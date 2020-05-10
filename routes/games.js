41c076dfbacb33561aa492ec2d4d946ae36e28a3
iths-anna-boberg <amboberg@gmail.com>
2020-05-10 18:58:55 +0200

    stats router

--- routes/games.js
+++ routes/games.js
@@ -1,5 +1,11 @@
 const {Router} = require('express');
 const router = new Router();
+const { db, fieldValue } = require ('./../firebase');
+
+
+
+
+
 
 
 module.exports = router;
--- routes/stats.js
+++ routes/stats.js
@@ -1,5 +1,28 @@
 const {Router} = require('express');
 const router = new Router();
+const { db, fieldValue } = require ('./../firebase');
 
+
+//GET Totalt antal matcher som hållits
+//hämtas från en db som innehåller en counter istället för att hämta hem hela matchdatabasen och räkna antalet objekt i den.
+router.get('/total', async (req,res)=>{
+
+    try{
+
+        let total = await db.collection('counter')
+                            .doc('totalGamesStats')
+                            .get()
+                            .then(doc => doc.data())
+
+                            res.status(200).send({games : total.totalGamesCount})
+
+
+    }catch(err){
+
+        res.status(500).send(err);
+
+    }
+
+})
 
 module.exports = router;
