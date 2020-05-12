f616f7c45ad2be05e00c8bae5b59e80cda5346f0
iths-anna-boberg <amboberg@gmail.com>
2020-05-12 15:24:14 +0200

    post ny game

--- routes/games.js
+++ routes/games.js
@@ -3,6 +3,44 @@ const router = new Router();
 const { db, fieldValue } = require ('./../firebase');
 
 
+/*POST Sparar en match med formatet 
+{   timeStamp: Date, 
+    contestants: 
+        [{ hamsterobject },
+         { hamsterobject } ]
+}
+---dock framgår det inte vem som är vinnare enligt den instruktionen? 
+eller jag döper dem till vinnare och förlorare?
+*/
+router.post('/', async (req, res)=>{
+    let date = new Date();
+    let d = date.getDate();
+    let m = date.getMonth();
+    let y = date.getFullYear();
+    let timeStamp = `${d}/${m+1}/${y}`;
+    try{
+        await db.collection('games')
+        .doc()
+        .set({
+            timestamp: timeStamp,
+            winner: req.body.winner,
+            loser: req.body.loser
+        })
+
+        await db.collection('counter') //separat collection för att räkna antal games, se router för stats
+        .doc('totalGamesStats')
+        .update({totalGamesCount : fieldValue.increment(1)})
+        .then(console.log('counter updated'))
+        
+        res.send({msg: 'New Game added and total games counter updated'})
+
+
+    }catch(err){
+        res.status(500).send(err)
+    }
+
+
+})
 
 
 
